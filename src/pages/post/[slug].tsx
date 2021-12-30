ed04e811038f0f3f15a6b1b41819204ef36d669e
João Paulo Fricks <jpfricks@gmail.com>
2021-12-29 23:33:36 -0300

    challenge completed 🚀

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,9 +28,57 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps): JSX.Element {
+  return (
+    <>
+      <div className={styles.container}>
+        <Header />
+      </div>
+
+      <img src="/banner.png" alt="imagem" className={styles.banner} />
+
+      <main className={commonStyles.container}>
+        <div className={styles.post}>
+          <div className={styles.postTop}>
+            <h1>xxx</h1>
+            <ul>
+              <li>
+                <FiCalendar />
+                {/* {post.first_publication_date} */}
+                29 Dez 2021
+              </li>
+              <li>
+                <FiUser />
+                {/* {post.data.author} */}
+                João
+              </li>
+              <li>
+                <FiClock />5 min
+              </li>
+            </ul>
+          </div>
+
+          <article>
+            <h2>xxxx sec</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+              saepe, tempora mollitia vero veniam ipsa quaerat cumque,
+              aspernatur, nihil animi nobis. Officiis, ipsa recusandae placeat,
+              animi commodi quidem maxime fuga culpa aspernatur distinctio
+              dolore vero dignissimos. <a href="/">Eius impedit</a> aperiam{' '}
+              <strong>quos</strong> debitis pariatur aspernatur alias totam
+              facere voluptatum <i>adipisci</i>
+              eos ipsam consequatur quo earum perferendis, commodi consequuntur
+              nihil sint? Dolore recusandae sed magni ut sint porro molestiae
+              minima neque dolor ullam cupiditate unde saepe blanditiis harum,
+              quibusdam ex praesentium impedit hic!
+            </p>
+          </article>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
