4610b421c857209ce55f324b0addb42b2da91d16
João Paulo Fricks <jpfricks@gmail.com>
2022-01-01 22:34:50 -0300

    challenge completed 🚀

---
+++ src/pages/api/exit-preview.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+export default async (_, res) => {
+  res.clearPreviewData();
+  res.writeHead(307, { Location: '/' });
+  res.end();
+};
