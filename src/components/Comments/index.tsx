737cc2d71daf3ae1ff1cb42002ead8317a95ab45
João Paulo Fricks <jpfricks@gmail.com>
2022-01-03 17:47:00 -0300

    challenge completed 🚀

---
+++ src/components/Comments/index.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Component, ReactElement } from 'react';
+
+// export default class Comments extends Component<ReactElement> {
+//   componentDidMount() {
+//     const script = document.createElement('script');
+//     const anchor = document.getElementById('inject-comments-for-uterances');
+//     script.setAttribute('src', 'https://utteranc.es/client.js');
+//     script.setAttribute('crossorigin', 'anonymous');
+//     script.setAttribute('async', 'true');
+//     script.setAttribute('repo', 'jpcmf/ig-ch-05');
+//     script.setAttribute('issue-term', 'pathname');
+//     script.setAttribute('theme', 'github-light');
+//     anchor.appendChild(script);
+//   }
+
+//   render() {
+//     return <div id="inject-comments-for-uterances" />;
+//   }
+// }
+
+export default function Comments() {
+  return (
+    <section
+      style={{ width: '100%' }}
+      ref={element => {
+        if (!element) {
+          return;
+        }
+
+        const scriptElement = document.createElement('script');
+        scriptElement.setAttribute('src', 'https://utteranc.es/client.js');
+        scriptElement.setAttribute('repo', 'jpcmf/ig-ch-05');
+        scriptElement.setAttribute('issue-term', 'pathname');
+        scriptElement.setAttribute('theme', 'github-dark');
+        scriptElement.setAttribute('crossorigin', 'anonymous');
+        scriptElement.setAttribute('async', 'true');
+        element.replaceChildren(scriptElement);
+      }}
+    />
+  );
+}
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -16,6 +16,7 @@ import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import Comments from '../../components/Comments';
 
 interface Post {
   first_publication_date: string | null;
@@ -106,6 +107,8 @@ export default function Post({ post, preview }: PostProps): JSX.Element {
             </article>
           ))}
         </div>
+
+        <Comments />
 
         {preview && (
           <aside>
